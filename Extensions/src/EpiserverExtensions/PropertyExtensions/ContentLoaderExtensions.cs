5c3f515d93a114ebe99a178bbdff55404ca8dec4
Vincent Yang <vincent.yang@episerver.com>
2020-08-04 10:34:31 +1000

    Add contentloader extension class

---
+++ Extensions/src/EpiserverExtensions/PropertyExtensions/ContentLoaderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EpiserverExtensions.Core.PropertyExtensions
+{
+    public static class ContentLoaderExtensions
+    {
+        public static IEnumerable<T> GetPublishedChildren<T>(this IContentLoader contentLoader, ContentReference contentLink) where T : IContent
+        {
+            var publishedStateAccessor = ServiceLocator.Current.GetInstance<IPublishedStateAssessor>();
+
+            return contentLoader.GetChildren<T>(contentLink).Where(x => publishedStateAccessor.IsPublished(x, PublishedStateCondition.None));
+        }
+    }
+}
